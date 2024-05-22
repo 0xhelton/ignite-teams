68566763266b42ffeaa1a7cd966837261a792547
0xhelton <0xhelton@gmail.com>
2024-05-21 21:13:20 -0300

    :package: build: add Expo Google Font to project dependencies

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
+import { ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components/native";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import theme from "./src/theme";
 
 import { Groups } from "@screens/Groups";
 
 export default function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Groups />
-    </ThemeProvider>
-  );
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return <ThemeProvider theme={theme}>{fontsLoaded ? <Groups /> : <ActivityIndicator />}</ThemeProvider>;
 }
