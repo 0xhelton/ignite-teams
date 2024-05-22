a51faaa6ce0d3e9a65684baed01115a47edd7ad4
0xhelton <0xhelton@gmail.com>
2024-05-22 20:43:14 -0300

    :sparkles: feat: add the Highlight component to reuse the structure of Title and Subtitle

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,13 @@
 import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+
 import { Container } from "./styles";
 
 export const Groups = () => {
   return (
     <Container>
       <Header />
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
     </Container>
   );
 };
