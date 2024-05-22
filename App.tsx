e4a6b3dc6f56383920c69fe29bfa87d74f1f051e
0xhelton <0xhelton@gmail.com>
2024-05-21 22:20:42 -0300

    :lipstick: feat: add Styles to Status Bar

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import theme from "./src/theme";
@@ -8,5 +9,10 @@ import { Loading } from "@components/Loading";
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
-  return <ThemeProvider theme={theme}>{!fontsLoaded ? <Groups /> : <Loading />}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
+  );
 }
