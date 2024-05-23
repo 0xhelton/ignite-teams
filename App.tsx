5f18c3d8c142253fd30b1d5134397a327446d3a7
0xhelton <0xhelton@gmail.com>
2024-05-23 20:40:55 -0300

    :sparkles: feat: add Players page and ButtonIcon component

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/
 import theme from "./src/theme";
 
 import { Loading } from "@components/Loading";
-import { NewGroup } from "@screens/NewGroup";
+import { Players } from "@screens/Players";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      {fontsLoaded ? <NewGroup /> : <Loading />}
+      {fontsLoaded ? <Players /> : <Loading />}
     </ThemeProvider>
   );
 }
