29ab7d051998e00802dfe631a89382e0806d8afb
0xhelton <0xhelton@gmail.com>
2024-05-23 18:10:29 -0300

    :sparkles: feat: add Button component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -7,6 +7,7 @@ import { GroupCard } from "@components/GroupCard";
 import { Container } from "./styles";
 import { FlatList } from "react-native";
 import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export const Groups = () => {
   const [groups, setGroups] = useState<string[]>([]);
@@ -23,6 +24,8 @@ export const Groups = () => {
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma?" />}
       />
+
+      <Button title="Criar nova turma"></Button>
     </Container>
   );
 };
