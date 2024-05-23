390960d40c6f4167a63efdb3c217adad55ce438f
0xhelton <0xhelton@gmail.com>
2024-05-23 18:49:00 -0300

    :sparkles: feat: add New Group page and Input component

--- App.tsx
+++ App.tsx
@@ -3,8 +3,8 @@ import { ThemeProvider } from "styled-components/native";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import theme from "./src/theme";
 
-import { Groups } from "@screens/Groups";
 import { Loading } from "@components/Loading";
+import { NewGroup } from "@screens/NewGroup";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <NewGroup /> : <Loading />}
     </ThemeProvider>
   );
 }
