36a10a82961b9766c28e506ffa850857374228fc
0xhelton <0xhelton@gmail.com>
2024-05-26 02:25:17 -0300

    :bug: fix: fix glitch on switch page and input text

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 
 import { Header } from "@components/Header";
@@ -8,10 +9,12 @@ import { Input } from "@components/Input";
 import { Container, Content, Icon } from "./style";
 
 export function NewGroup() {
+  const [group, setGroup] = useState("");
+
   const navigation = useNavigation();
 
   function handleNew() {
-    navigation.navigate("players", { group: "Rocket" });
+    navigation.navigate("players", { group: group });
   }
 
   return (
@@ -22,7 +25,7 @@ export function NewGroup() {
         <Icon />
 
         <Highlight title="Nova turma" subtitle="crie a turma para adicionar as pessoas" />
-        <Input placeholder="Nome da turma" />
+        <Input placeholder="Nome da turma" onChangeText={setGroup} />
 
         <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
