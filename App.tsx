fdd4090f11db5db4767883d19aa6c8c09375627c
0xhelton <0xhelton@gmail.com>
2024-05-26 02:08:08 -0300

    :sparkles: feat: add navigation system and handle functions

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from "react-native";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 
 import { Loading } from "@components/Loading";
-import { Players } from "@screens/Players";
+import { Routes } from "./src/routes";
 
 import { ThemeProvider } from "styled-components/native";
 import theme from "./src/theme";
@@ -13,7 +13,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from "@react-navigation/native";
+
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
@@ -6,6 +8,12 @@ import { Input } from "@components/Input";
 import { Container, Content, Icon } from "./style";
 
 export function NewGroup() {
+  const navigation = useNavigation();
+
+  function handleNew() {
+    navigation.navigate("players", { group: "Rocket" });
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -16,7 +24,7 @@ export function NewGroup() {
         <Highlight title="Nova turma" subtitle="crie a turma para adicionar as pessoas" />
         <Input placeholder="Nome da turma" />
 
-        <Button title="Criar" style={{ marginTop: 20 }} />
+        <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
   );
