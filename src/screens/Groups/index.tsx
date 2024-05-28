7dca338693ca5b6208d9c00bff1a0daa5c154110
0xhelton <0xhelton@gmail.com>
2024-05-28 20:29:01 -0300

    :sparkles: feat: add useFocusEffect for data persistence with AsyncStorage

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { FlatList } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
@@ -9,6 +9,7 @@ import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
 
 import { Container } from "./styles";
+import { groupGetAll } from "@storage/groupGetAll";
 
 export const Groups = () => {
   const [groups, setGroups] = useState<string[]>([]);
@@ -18,6 +19,21 @@ export const Groups = () => {
   function handleNewGroup() {
     navigation.navigate("new");
   }
+
+  async function fetchGroups() {
+    try {
+      const data = await groupGetAll();
+      setGroups(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchGroups();
+    }, [])
+  );
 
   return (
     <Container>
