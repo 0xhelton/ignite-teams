24917d5fd9f2edfc36fcdd21549b5398b4c80e7d
0xhelton <0xhelton@gmail.com>
2024-05-28 20:58:45 -0300

    :sparkles: feat: now its possible to open created groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -29,6 +29,10 @@ export const Groups = () => {
     }
   }
 
+  function handleOpenGroup(group: string) {
+    navigation.navigate("players", { group });
+  }
+
   useFocusEffect(
     useCallback(() => {
       fetchGroups();
@@ -43,7 +47,7 @@ export const Groups = () => {
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <GroupCard title={item} />}
+        renderItem={({ item }) => <GroupCard title={item} onPress={() => handleOpenGroup(item)} />}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma?" />}
         showsVerticalScrollIndicator={false}
