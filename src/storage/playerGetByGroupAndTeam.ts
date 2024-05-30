a936cb8e42f7b1e92d396e8a2f4f1e66f2034294
0xhelton <0xhelton@gmail.com>
2024-05-29 23:58:08 -0300

    :bug: fix: fix missing return players

--- src/storage/playerGetByGroupAndTeam.ts
+++ src/storage/playerGetByGroupAndTeam.ts
@@ -5,6 +5,8 @@ export async function playerGetByGroupAndTeam(group: string, team: string) {
     const storage = await playerGetByGroup(group);
 
     const players = storage.filter((player) => player.team === team);
+
+    return players;
   } catch (error) {
     throw error;
   }
